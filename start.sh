39ae4a2fe00e39aba20d8116905e1245b01e0f27
최용재 <yongjae@zigbang.com>
2020-01-23 18:29:55 +0900

    redis가 완전히 켜질때까지 기다리고나서 클러스터 구성

--- start.sh
+++ start.sh
@@ -4,5 +4,7 @@ redis-server redis1.conf &
 redis-server redis2.conf & 
 redis-server redis3.conf &
 
+sleep 3
+
 redis-cli --cluster create 127.0.0.1:6379 127.0.0.1:6380 127.0.0.1:6381 --cluster-replicas 0
 redis-cli cluster info | grep state
